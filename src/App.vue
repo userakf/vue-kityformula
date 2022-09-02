ca96ac823723f33e6610e5eb6bb8c2616d296bb3
userakf <820279897@qq.com>
2022-09-02 21:34:39 +0800

    chore:配置

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <kityformulaVue />
+    </div>
+</template>
+
+<script setup lang="ts">
+import kityformulaVue from './components/kityformula.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/kityformula.vue
@@ -0,0 +1,22 @@
+<template>
+    <vue-ueditor-wrap v-model="msg" :config="config"></vue-ueditor-wrap>
+
+    <p>
+        {{ msg }}
+    </p>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue';
+import VueUeditorWrap from 'vue-ueditor-wrap'
+const msg = ref()
+const config = reactive({
+    toolbars:[
+        ['']
+    ]
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
